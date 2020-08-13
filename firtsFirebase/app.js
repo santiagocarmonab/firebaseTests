6aa9d5df5785e4940889234af046c92fe5529bed
santiagocarmoa <santiago.carmonab@udea.edu.co>
2020-08-12 19:22:55 -0500

    Se han modificado los archivos, ahora no se muestra la lista completa de la base de datos, en esta ocasion se pone como primer elemento de la lista el ultimo texto que se haya escrito en la linea de texto

--- firtsFirebase/app.js
+++ firtsFirebase/app.js
@@ -7,9 +7,21 @@ firebase.initializeApp({
   
 var db = firebase.firestore();
   
-$(document).ready(function(){
-   
+$(document).ready(async function(){
+
     counter=1;
+    try{
+        const ultimo = await db.collection("test2").orderBy("createdAt", "desc").limit(1).get();
+        console.log(ultimo)
+        ultimo.forEach((doc) => {   
+            console.log(`${doc.data().text}`);
+            $('#firstText').html(`${doc.data().text}`);
+        })
+    }
+    catch(error){
+        console.log("paila")
+    }
+    
     $('#newElement').click(function(){
         var element = $("<li></li>").text("You added item: " + counter);
         element.addClass("display-4");
@@ -17,35 +29,26 @@ $(document).ready(function(){
         counter ++;
     })
 // send input data to data base
-    $('#changeText').click(function(){
-        var inputData = $('#changed').val();
-        $('#firstText').html(inputData);
-        db.collection("test1").add({
-            data : inputData
-        })
-        .then(function(docRef) {
+    $('#changeText').click(async function(){ 
+        try{
+            var inputData = $('#changed').val();
+            $('#firstText').html(inputData);
+            const docRef= await db.collection("test2").add({
+                text : inputData,
+                createdAt : new Date().getTime()
+            })
+            
             console.log("Document written with ID: ", docRef.id);
             $('#changed').val("");
-        })
-        .catch(function(error) {
+        }
+        catch(error){
             console.error("Error adding document: ", error);
-        });
+        }
     });
     
     $('#changeColor').click(function(){
         
         $('#title1').addClass("text-danger");
         $('#title2').addClass("text-success");
-    });
-});
-
-//Show old data saved on data base
-var table =$('#myTable');
-db.collection("test1").get().then((querySnapshot) => {
-    table.val("");
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().data}`);
-        var toAdd = $("<tr></tr>").text(`${doc.data().data}`);
-        $('#myTable').append(toAdd);
     });
 });
